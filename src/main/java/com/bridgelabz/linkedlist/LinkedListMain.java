60e5a6ffa51d56c0a2917039ce7b3cc3ae473083
megha angadi <meghamangadi@gmail.com>
2021-09-09 00:01:40 +0530

    Added: Welcome Message To The User

---
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+
+public class LinkedListMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to DataStructure LinkedList Program" );
+    }
+}
