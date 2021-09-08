d2f340bbed691c6b9df21c6ca0dbfecaaacea4c5
megha angadi <meghamangadi@gmail.com>
2021-09-09 01:08:59 +0530

    Added: Created LinkedList by adding data from first data by Append

--- src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -7,13 +7,18 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to linkedList DataStructure Program");
 
+		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter 1 to add the data at the start");
+        System.out.println("Enter 1 to add the data at the start");
+        System.out.println("Enter 2 to add the data at the end");
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addeElementAtStart();
+                break;
+            case 2:
+                Operations.addElementAtEnd();
+                break;
+        }
 
-		switch (scanner.nextInt()) {
-		case 1:
-			Operations.addeElementAtStart();
-		}
-
-	}
+    }
 }
