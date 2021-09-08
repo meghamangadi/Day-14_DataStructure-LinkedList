c72b4d16e5d27d427e814ec6f13de83e105dd1f1
megha angadi <meghamangadi@gmail.com>
2021-09-09 00:13:02 +0530

    Added: Created LinkedList of data 56,30 & 70

--- src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -6,5 +6,18 @@ public class LinkedListMain
     public static void main( String[] args )
     {
         System.out.println( "Welcome to DataStructure LinkedList Program" );
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
